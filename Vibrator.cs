884143443c84b4721436959d7c23d21328bc5daa
Sherry <84338798+lemesherry@users.noreply.github.com>
2022-04-06 23:56:05 -0700

    Update Vibrator.cs

--- Vibrator.cs
+++ Vibrator.cs
@@ -4,28 +4,40 @@ namespace Sherry.CustomScripts {
 
     public class Vibrator {
 
+        private static class AndroidSet {
+
+            public const string unityPlayer = "com.unity3d.player.UnityPlayer";
+            public const string currentActivity = "currentActivity";
+            public const string getSystemService = "getSystemService";
+            public const string vibrator = "vibrator";
+            public const string vibrate = "vibrate";
+            public const string cancel = "cancel";
+
+        }
+    
     #if UNITY_ANDROID
-        private static AndroidJavaClass unityPlayer = new AndroidJavaClass( AndroidSet.unityPlayer );
-        private static AndroidJavaObject currentActivity = unityPlayer.GetStatic< AndroidJavaObject >( AndroidSet.currentActivity );
-        private static AndroidJavaObject vibrator = currentActivity.Call< AndroidJavaObject >( AndroidSet.getSystemService, AndroidSet.vibrator );
+        private static readonly AndroidJavaClass UnityPlayer = new AndroidJavaClass( AndroidSet.unityPlayer );
+        private static readonly AndroidJavaObject CurrentActivity = UnityPlayer.GetStatic< AndroidJavaObject >( AndroidSet.currentActivity );
+        private static readonly AndroidJavaObject AndroidVibrator = CurrentActivity.Call< AndroidJavaObject >( AndroidSet.getSystemService, AndroidSet.vibrator );
     #else
         private static AndroidJavaClass unityPlayer;
         private static AndroidJavaObject currentActivity;
         private static AndroidJavaObject vibrator;
     #endif
 
-        public void Vibrate( long vibrationMilliseconds = 80 ) {
+        public static void Vibrate( long vibrationMilliseconds = 80 ) {
 
-            if( IsAndroidDevice() ) vibrator.Call( AndroidSet.vibrate, vibrationMilliseconds );
+            Debug.Log( "Vibrator is Called" );
+            if( IsAndroidDevice() ) AndroidVibrator.Call( AndroidSet.vibrate, vibrationMilliseconds );
             else Handheld.Vibrate();
         }
 
         public void CancelVibration() {
 
-            if( IsAndroidDevice() ) vibrator.Call( AndroidSet.cancel );
+            if( IsAndroidDevice() ) AndroidVibrator.Call( AndroidSet.cancel );
         }
 
-        private bool IsAndroidDevice() {
+        private static bool IsAndroidDevice() {
 
         #if UNITY_ANDROID && !UNITY_EDITOR
                 return true;
@@ -36,16 +48,4 @@ namespace Sherry.CustomScripts {
 
     }
 
-            
-    public class AndroidSet {
-
-        public const string unityPlayer = "com.unity3d.player.UnityPlayer";
-        public const string currentActivity = "currentActivity";
-        public const string getSystemService = "getSystemService";
-        public const string vibrator = "vibrator";
-        public const string vibrate = "vibrate";
-        public const string cancel = "cancel";
-
-
-    }
 }
